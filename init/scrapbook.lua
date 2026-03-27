502ce3a77989a02ebdc982e2fbd2dadb5bba207f
Kyno <40477759+ImKyno@users.noreply.github.com>
2026-03-26 23:48:15 -0300

    Added Scrapbook Unlockability

--- init/scrapbook.lua
+++ init/scrapbook.lua
@@ -9,3 +9,11 @@ for k, v in pairs(SCRAPBOOK_GNOMES) do
     scrapbook_prefabs[k] = true
     scrapbookdata[k]     = v
 end
+
+AddPlayerPostInit(function(inst)
+    if inst and _G.TheScrapbookPartitions then
+        for k, v in pairs(SCRAPBOOK_GNOMES) do
+            _G.TheScrapbookPartitions:SetViewedInScrapbook(k)
+        end
+    end
+end)
